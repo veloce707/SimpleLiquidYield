414c8803cea2dfe142b1fbf3b1f96ce48bd2dfc0
veloce707 <veloce707@protonmail.com>
2026-01-23 07:54:56 -0500

    initial commit

---
+++ contracts/slyUSD.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.5.0
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SlyUSD is ERC20, Ownable {
+    constructor(address initialOwner)
+        ERC20("SlyUSD", "slyUSD")
+        Ownable(initialOwner)
+    {}
+
+    address public USDC;
+    address public depositHandler;
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
